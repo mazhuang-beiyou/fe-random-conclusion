051ea07a17771519987bcdb30f9071de326559d0
zhaoxingyu <zhaoxingyu@gitlab.com>
2021-01-11 15:39:38 +0800

    gitignore

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-var value = 1
-const foo = {
-    value: 2
+function fun(n,o){
+    console.log(o);
+    return {
+        fun: function(m){
+            return fun(m,n);
+        }
+    };
 }
-function fn() {
-    console.log(this.value)
-    return this.value
-}
-
-Function.prototype.bind2 = function (context) {
-    const fn = this
-    return function() {
-        return fn.apply(context)
-    }
-}
-bindFoo = fn.bind2(foo)
-console.log(bindFoo()) //2 ,2
+const a = fun(0)
+a.fun(1) // 0
