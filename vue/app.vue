0d4405dcb6b6d8fbede1f6dc39dbd5982123a6b6
zhaoxingyu <zhaoxingyu@gitlab.com>
2021-01-11 15:37:33 +0800

    单文件vue测试

---
+++ vue/app.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+        指令测试
+        <input v-focus type="text">
+    </div>
+</template>
+<script>
+import Vue from 'vue'
+Vue.directive('focus', {
+  // When the bound element is inserted into the DOM...
+  inserted: function (el) {
+    // Focus the element
+    el.focus()
+  }
+})
+export default {
+    data() {
+        return {};
+    }
+};
+</script>
+
+<style scoped>
+</style>
